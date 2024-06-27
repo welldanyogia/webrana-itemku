e715fbb8930b81dd1bb4e7fe89c7d95d642aee25
welldanyogia <welldanyogia01@gmail.com>
2024-06-27 13:25:30 +0700

    init

--- app/dashboard/(products)/brand/[slug]/column.tsx
+++ app/dashboard/(products)/brand/[slug]/column.tsx
@@ -61,10 +61,10 @@ export const columns: ColumnDef<Product>[] = [
             const product = row.original;
             return <span>{product.brand?.brand_name}</span>;
         },
-        filterFn: (row, id, value) => {
-            const brandName = row.getValue(id)?.brand_name;
-            return value.includes(brandName);
-        },
+        // filterFn: (row, id, value) => {
+        //     const brandName = row.getValue(id)?.brand?.brannd;
+        //     return value.includes(brandName);
+        // },
     },
     {
         accessorKey: "type",
