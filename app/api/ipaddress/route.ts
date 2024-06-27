429e99a2c2daa9585a5fba1a54c0bb7a1f002157
welldanyogia <welldanyogia01@gmail.com>
2024-06-27 13:08:42 +0700

    init

--- app/api/ipaddress/route.ts
+++ app/api/ipaddress/route.ts
@@ -1,21 +1,22 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import os from 'os';
-import {NextRequest, NextResponse} from "next/server";
 
 const getServerIP = () => {
     const interfaces = os.networkInterfaces();
     for (const interfaceName in interfaces) {
         const networkInterface = interfaces[interfaceName];
-        for (const interfaceInfo of networkInterface) {
-            if (interfaceInfo.family === 'IPv4' && !interfaceInfo.internal) {
-                return interfaceInfo.address;
+        if (networkInterface) { // Ensure networkInterface is defined
+            for (const interfaceInfo of networkInterface) {
+                if (interfaceInfo.family === 'IPv4' && !interfaceInfo.internal) {
+                    return interfaceInfo.address;
+                }
             }
         }
     }
     return null;
 };
 
-export async function POST(req : NextRequest, res : NextApiResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
     const serverIP = getServerIP();
     return NextResponse.json({ serverIP });
 }
